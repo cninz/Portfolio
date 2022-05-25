ac37089a1aa44e05dd23b171e03bc07b8594caa2
Nirel Chua <95299412+cninz@users.noreply.github.com>
2022-05-25 00:45:07 -0600

    Edit

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -30,10 +30,11 @@
 
 
 const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
 
 const appearOptions = {
     threshold: 0,
-    rootMargin: "0px 0px -150px 0px"
+    rootMargin: "0px 0px -250px 0px"
 };
 
 const appearOnScroll = new IntersectionObserver(function (
