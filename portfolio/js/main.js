e2540ba9918bbf80a525cb9dfad417142ff43b9e
Nirel Chua <95299412+cninz@users.noreply.github.com>
2022-05-29 12:19:43 -0600

    Edit

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,33 +1,12 @@
-// const faders = document.querySelectorAll('.fade-in'); 
-
-// sectionOneObserver.observe(sectionOne);
-
-// const appearOptions = {
-//     threshold: 1,
-//     rootMargin: "0px 0px -100px 0px"
-// };
-
-// const appearOnScroll = new IntersectionObserver
-// (function(
-//     entries,
-//     appearOnScroll
-// ) {
-//     entries.forEach(entry => {
-//         if(!entry.isIntersecting) {
-//             return;
-//         }else{
-//             entry.target.classList.add('appear');
-//             appearOnScroll.unobserver(entry.target);
-//         }
-//     });
-// }, 
-// appearOptions);
-
-// faders.forEach(fader => {
-//     appearOnScroll.observe(fader);
-// })
 
 
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
 
 const faders = document.querySelectorAll('.fade-in');
 const sliders = document.querySelectorAll('.slide-in');
